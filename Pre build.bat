d317a5c73ac1d52071d52d0e7bf200c61918a76d
suxiaojack <34434940@qq.com>
2015-08-13 10:38:31 +0800

    first run Pre build.bat to copy need files to release directory

---
+++ Pre build.bat
@@ -0,0 +1,5 @@
+
+REM Update language files
+copy "%~dp0Language files\*" "%~dp0Bin\Debug\lang\*" 
+copy "%~dp0Language files\*" "%~dp0Bin\Todomoo\lang\*" 
+copy "%~dp0Language files\*" "%~dp0Bin\Empty bin folder\lang\*" 
